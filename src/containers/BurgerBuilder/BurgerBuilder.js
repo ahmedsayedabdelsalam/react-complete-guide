00e446531a3bd0c7ec5bae4b7173d35be1f95808
ahmed sayed abdelsalam <e.eng.ahmedsayed@gmail.com>
2020-01-04 14:54:13 +0200

    store order in firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -34,7 +35,23 @@ class BurgerBuilder extends Component {
   }
 
   purchasingContinueHandler = () => {
-    alert('Purchasing Continued !')
+    const order = {
+      ingredients: this.state.ingredients,
+      totalPrice: this.state.totalPrice,
+      customer: {
+        name: 'Ahmed Sayed Abdelsalam',
+        address: {
+          street: 'my street 1',
+          zipCode: '12345',
+          country: 'Egypt'
+        }
+      },
+      deliveryMethod: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
   }
 
   updatepurchaseable = ingredients => {
